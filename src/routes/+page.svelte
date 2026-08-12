9f2841666d272413b22ea5fcee4403c6002e9d07
dontwanttothink <git@dontwanttoth.ink>
2026-08-11 23:14:29 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { buildTimeEmojis, buildTimeEmojiInfo } from "./example.remote";
+</script>
+
+{#each await buildTimeEmojis() as emoji}
+<h2>{emoji}</h2>
+<p>{await buildTimeEmojiInfo(emoji)}</p>
+{/each}
---
+++ src/routes/example.remote.ts
@@ -0,0 +1,25 @@
+import * as v from "valibot";
+import { prerender } from "$app/server";
+
+export const buildTimeEmojis = prerender(async () => {
+	if (Date.now() % 2 === 0) {
+		return ["😢", "😭", "🐮", "🤠", "🦧"];
+	} else {
+		return ["🪄", "❓", "👩‍⚕️", "🥲", "❤️"];
+	}
+});
+
+async function fetchDescriptionFromFileSystem(emoji: string) {
+	// imagine this actually fetches a description from the file system
+	// or some other source available only at build time
+	return "this emoji means something related to " + emoji;
+}
+
+export const buildTimeEmojiInfo = prerender(
+	v.string(),
+	async (emoji: string) => {
+		const description = await fetchDescriptionFromFileSystem(emoji);
+		return `the emoji ${emoji} means ${description}!`;
+	},
+	{ inputs: async () => await buildTimeEmojis() },
+);
